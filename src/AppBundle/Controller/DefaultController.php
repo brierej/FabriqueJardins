cdcb4dbdba361655da2249ebfb81b463e7584955
ORIENTIS\j.briere <j.briere@orientis.fr>
2017-12-11 12:08:49 +0100

    fix : homepage router on /

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,17 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
+     * @Route("/", name="homepage")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
      * @Route("/hp1", name="homepage1")
      */
     public function indexAction(Request $request)
